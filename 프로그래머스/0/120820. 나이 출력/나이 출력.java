3f4e23d9e40be7157a22030414ae5faccacd5b58
DWChoo <103016339+DongWooChoo@users.noreply.github.com>
2025-05-15 20:20:12 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int age) throws IOException {
+        int year = 2022; // 기준 연도
+        return year - age + 1;
+    }
+}
